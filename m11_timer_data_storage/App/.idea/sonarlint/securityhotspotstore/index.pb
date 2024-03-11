57fa9fe0b75cd442e69316ab67cf1b8662dfe3e0
nikitam1337 <nikitam.l@mail.ru>
2024-03-11 18:37:57 +0300

    m11 start

---
+++ m11_timer_data_storage/App/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+b
+2app/src/main/java/com/example/hw11/MainActivity.kt,6\9\693cc2f767092d226fd03ab132b8edce1b4e0d6a
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
---
+++ m11_timer_data_storage/App/app/src/main/java/com/example/hw11/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.hw11
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hw11.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() {
+            return _binding!!
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
